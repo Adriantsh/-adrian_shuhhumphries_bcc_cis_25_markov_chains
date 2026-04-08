bcc003ce8d14514f0a3bf1fcba7e52d85d7f5ae3
Adrian Shuh-Humphries <ashuhhum@ucsc.edu>
2026-04-08 16:16:38 -0700

    The readWordsFromFile function works as intented.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@ using namespace std;
 
 int main()
 {   
-    srand(time(0));
-    string testWords[] = {"the", "cat", "sat", "down"};
-    cout << joinWords(testWords, 0, 2) << std::endl;  // Should print: the cat
-    cout << joinWords(testWords, 1, 3) << std::endl;  // Should print: cat sat down
+    string words[1000];
+    int count = readWordsFromFile("test.txt", words, 1000);
+    cout << "Read " << count << " words" << endl;
+    for (int i = 0; i < 10 && i < count; i++) 
+    {
+        cout << words[i] << endl;
+    }
 }
