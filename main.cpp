d3c9839cfc9cb353f76d2d55efa7fbd34a94a59f
Adrian Shuh-Humphries <ashuhhum@ucsc.edu>
2026-04-08 16:59:36 -0700

    completed main program

--- main.cpp
+++ main.cpp
@@ -14,5 +14,9 @@ int main()
     string prefixes[10000], suffixes[10000];
     int chainSize = buildMarkovChain(words, count, 1, prefixes, suffixes, 10000);
 
+    string output = generateText(prefixes, suffixes, chainSize, 1, 20);
+    cout << output << endl;
+
+
     return 0;
 }
